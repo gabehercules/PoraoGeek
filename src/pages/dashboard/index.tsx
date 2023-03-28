046cd29565c8321b3c1f6437d36029de72b7947d
Gabriel <gabrielsyze@gmail.com>
2023-03-27 21:47:42 -0300

    test in dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -13,27 +13,7 @@ export default function Dashboard({ posts }: { posts: any[] }) {
             <FeaturedBanner />
             <Notifications />
             <div>
-              <ul className="flex flex-wrap gap-4">
-                {posts.map((post) => (
-                  <li key={post.id} className="flex flex-1">
-                    <a
-                      href={post.link}
-                      className="w-[300px] flex flex-1  flex-col border border-dark-border rounded-md overflow-hidden"
-                    >
-                      <div className="h-[120px] bg-dark-border flex items-center justify-center text-white/40 text-xl font-bold">
-                        Thumb
-                      </div>
-                      <div className="p-2">
-                        <h2 className="text-white">{post.title.rendered}</h2>
-                        <span className="flex items-center gap-1 text-sm text-dark-text mt-2">
-                          <ShardIcon />
-                          +12
-                        </span>
-                      </div>
-                    </a>
-                  </li>
-                ))}
-              </ul>
+              <ul className="flex flex-wrap gap-4">ola</ul>
             </div>
           </div>
           <div className="bg-brand-green flex-1">side panel</div>
@@ -41,17 +21,4 @@ export default function Dashboard({ posts }: { posts: any[] }) {
       </Layout>
     </>
   );
-}
-
-export async function getStaticProps() {
-  const data = await fetch(
-    "https://poraogeek.com.br/wp-json/wp/v2/posts?per_page=6"
-  );
-  const posts = await data.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
 }
