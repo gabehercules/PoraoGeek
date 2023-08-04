a4c23830bfee1963a45ddfc04005e95f64c3a15f
Gabriel <gabrielsyze@gmail.com>
2023-08-04 01:05:20 -0300

    reassigned domains

--- src/app/(public)/games/page.tsx
+++ src/app/(public)/games/page.tsx
@@ -1,10 +1,13 @@
 export default function Page() {
   return (
-    <>
-      <div>
-        <div>banner</div>
-        <div>games</div>
+    <div className="w-full p-10">
+      <div className="flex flex-col items-center justify-center w-full h-[240px] rounded-lg bg-dark-secondary">
+        <h1 className="text-dark-text text-lg font-semibold mb-2">Em breve</h1>
+        <p>
+          Em breve, um local para montar grupos de jogos. Encontrar grupos
+          baseados em títulos de jogos, categoria de jogos e muito mais
+        </p>
       </div>
-    </>
+    </div>
   );
 }
--- src/app/(public)/mercado/page.tsx
+++ src/app/(public)/mercado/page.tsx
@@ -5,7 +5,7 @@ export default function Mercado() {
     <div className="w-full p-10">
       <div className="flex flex-col items-center justify-center w-full h-[240px] rounded-lg bg-dark-secondary">
         <h1 className="text-dark-text text-lg font-semibold mb-2">Em breve</h1>
-        <p>Nem eu sei o que isso vai ser kk</p>
+        <p>O time de produto ainda não definiu o que vai ser aqui</p>
       </div>
     </div>
   );
